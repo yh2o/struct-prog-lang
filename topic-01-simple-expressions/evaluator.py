4bdfb90473ebfc298e1a4bc6b01575ba10b0313a
Yusairah H <yusairah.haque@gmail.com>
2026-02-23 16:49:23 -0500

    update

--- topic-01-simple-expressions/evaluator.py
+++ topic-01-simple-expressions/evaluator.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-#evaluator.py 
-import parser, tokenizer
-
-def evaluate(ast):
-    if ast["tag"] == "number": 
-=======
 import parser, tokenizer
 
 def evaluate(ast):
     if ast["tag"] == "number":
->>>>>>> 3d1af22a86d3ce350ec311a5c62629e5814848ec
         return ast["value"]
     elif ast["tag"] == "+":
         return evaluate(ast["left"]) + evaluate(ast["right"])
@@ -19,11 +11,8 @@ def evaluate(ast):
         return evaluate(ast["left"]) * evaluate(ast["right"])
     elif ast["tag"] == "/":
         return evaluate(ast["left"]) / evaluate(ast["right"])
-<<<<<<< HEAD
     elif ast["tag"] == "%":
         return evaluate(ast["left"]) % evaluate(ast["right"])
-=======
->>>>>>> 3d1af22a86d3ce350ec311a5c62629e5814848ec
     else:
         raise ValueError(f"Unknown AST node: {ast}")
 
@@ -34,11 +23,7 @@ def test_evaluate():
     ast = {
         "tag": "+",
         "left": {"tag": "number", "value": 3},
-<<<<<<< HEAD
-        "right": {"tag": "number", "value": 4}
-=======
         "right": {"tag": "number", "value": 4},
->>>>>>> 3d1af22a86d3ce350ec311a5c62629e5814848ec
     }
     assert evaluate(ast) == 7
     ast = {
@@ -51,7 +36,6 @@ def test_evaluate():
         "right": {"tag": "number", "value": 5},
     }
     assert evaluate(ast) == 35
-<<<<<<< HEAD
     ast = {
         "tag": "%",
         "left": {"tag": "number", "value": 5},
@@ -85,12 +69,3 @@ def test_evaluate():
 if __name__ == "__main__":
     test_evaluate()
     print("done.")
-=======
-    tokens = tokenizer.tokenize("3*(4+5)")
-    ast, tokens = parser.parse_expression(tokens)
-    assert evaluate(ast) == 27
-
-if __name__ == "__main__":
-    test_evaluate()
-    print("done.")
->>>>>>> 3d1af22a86d3ce350ec311a5c62629e5814848ec
